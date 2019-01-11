3beea619d168cc362fdaeb27af57cf5cffe5b7d4
Hendi Santika <hendisantika@gmail.com>
2019-01-12 05:16:04 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootdtotestsample/SpringBootDtoTestSampleApplication.java
@@ -0,0 +1,14 @@
+package com.hendisantika.springbootdtotestsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDtoTestSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDtoTestSampleApplication.class, args);
+    }
+
+}
+
